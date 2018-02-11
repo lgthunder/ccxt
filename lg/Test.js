d3e9ce59c9ae8876eb6a5ec43bc06460914a9b80
leiting <leiting@leitingdeMacBook-Pro.local>
2018-02-11 16:52:47 +0800

    fix proxy

--- lg/Test.js
+++ lg/Test.js
@@ -30,27 +30,24 @@ async function getAccount() {
 }
 
 let http = require('http')
-let  opt = {
-    host:'47.52.158.231',
-    port:'8191',
-    method:'GET',//这里是发送的方法
-    path:'http://www.baidu.com',     //这里是访问的路径
-    headers:{
+let opt = {
+    method: 'GET',//这里是发送的方法
+    headers: {
         //这里放期望发送出去的请求头
     }
 }
 //以下是接受数据的代码
 var body = '';
-var req = http.request(opt, function(res) {
+var req = http.request(opt, function (res) {
     console.log("Got response: " + res.statusCode);
-    res.on('data',function(d){
+    res.on('data', function (d) {
         body += d;
-    }).on('end', function(){
+    }).on('end', function () {
         console.log(res.headers)
         console.log(body)
     });
 
-}).on('error', function(e) {
+}).on('error', function (e) {
     console.log("Got error: " + e.message);
 })
 req.end();
