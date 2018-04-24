10ed6f5c892248a92c4be9e79a9c62a0535e4541
leiting <leiting@leitingdeMBP.lan>
2018-04-24 21:56:38 +0800

    db last1

--- examples/js/trade.js
+++ examples/js/trade.js
@@ -333,7 +333,7 @@ module.exports = class huobitrade {
     async getMyPosition(save) {
         let position = this.myPosition();
         let db = this.getDb();
-        db.get("last", function (res) {
+        db.getObj("last", function (res) {
             position(res, save)
         })
     }
